1ecb1067347e1a43135aadf4f482d9c526cad90e
lendl-uy <125571135+lendl-uy@users.noreply.github.com>
2023-03-08 22:32:43 +0800

    Update mpc.py

--- mpc/mpc.py
+++ mpc/mpc.py
@@ -19,111 +19,161 @@ import numpy as np
 from mpc_params import *
 import traffic_distribution as td
 
-# Initialize a model
-m = gp.Model("MPC")
+def do_mpc():
 
-# Initialize GUROBI decision variables
+  # Initialize a model
+  m = gp.Model("MPC")
 
-# x: vehicle count
-# u: green time
-x = m.addMVar(shape=(N+1,4), lb=xmin, vtype=GRB.INTEGER, name="x")
-u = m.addMVar(shape=(N,4), lb=umin, vtype=GRB.INTEGER, name="u")
+  # Initialize GUROBI decision variables
 
-# C: cycle time
-# C_dummy: to be used for division
-C = m.addVar(vtype=GRB.INTEGER, lb=20.0, name="C")
-C_dummy = m.addVar(vtype=GRB.CONTINUOUS, name="C_dummy")
+  # x: vehicle count
+  # u: green time
+  x = m.addMVar(shape=(N+1,4), lb=xmin, vtype=GRB.INTEGER, name="x")
+  u = m.addMVar(shape=(N,4), lb=umin, vtype=GRB.INTEGER, name="u")
 
-# u_41 and u_43 divides sum of green times in Aurora Blvd. East
-# Aurora Blvd. East has two phases, to Katipunan Ave. South and Aurora Blvd. West
-u_41 = m.addVar(vtype=GRB.INTEGER, lb=5.0, name="u_41")
-u_43 = m.addVar(vtype=GRB.INTEGER, lb=5.0, name="u_43")
+  # C: cycle time
+  # C_dummy: to be used for division
+  C = m.addVar(vtype=GRB.INTEGER, lb=20.0, name="C")
+  C_dummy = m.addVar(vtype=GRB.CONTINUOUS, name="C_dummy")
 
-# Intermediary variables to compute difference of x(k+ko|ko) and u(k+ko|ko)
-y = m.addMVar(shape=(N+1,4), lb=-GRB.INFINITY, vtype=GRB.CONTINUOUS, name="y") # x(k_o+k)-x(k_o)
-z = m.addMVar(shape=(N,4), lb=-GRB.INFINITY, vtype=GRB.CONTINUOUS, name="z") # u(k_o+k)-u(k_o)
+  # u_41 and u_43 divides sum of green times in Aurora Blvd. East
+  # Aurora Blvd. East has two phases, to Katipunan Ave. South and Aurora Blvd. West
+  u_41 = m.addVar(vtype=GRB.INTEGER, lb=5.0, name="u_41")
+  u_43 = m.addVar(vtype=GRB.INTEGER, lb=5.0, name="u_43")
 
-# Constraints
-m.addConstr(x[0, :] == np.array([0,0,0,0])) # Initial number of vehicles
-for k in range(N):
+  # Intermediary variables to compute difference of x(k+ko|ko) and u(k+ko|ko)
+  y = m.addMVar(shape=(N+1,4), lb=-GRB.INFINITY, vtype=GRB.CONTINUOUS, name="y") # x(k_o+k)-x(k_o)
+  z = m.addMVar(shape=(N,4), lb=-GRB.INFINITY, vtype=GRB.CONTINUOUS, name="z") # u(k_o+k)-u(k_o)
 
-  # Traffic model
-  m.addConstr(x[k+1, :] == x[k, :] + u[k, :] @ B*C_dummy + D[k, :]) # (Bu(k))^T = u(k)^T B^T
+  # Constraints
+  m.addConstr(x[0, :] == np.array([0,0,0,0])) # Initial number of vehicles
+  for k in range(N):
 
-  # Constraints in order to obey existing phases
-  m.addConstr(u[k, 0] == u[k, 1]) # Green time of Katipunan Ave North and South must be the same
-  m.addConstr(u[k, 3] == u_41+u_43) # Aurora East has 2 green times/phases
-  m.addConstr(u[k, 2] == u_43-u_41-3) # Green time of Aurora West
-  m.addConstr(C == u[k, 0] + u_43 + 6) # Total cycle time is equal to phase 1 + phase 2 + phase 3
+    # Traffic model
+    m.addConstr(x[k+1, :] == x[k, :] + u[k, :] @ B*C_dummy + D[k, :]) # (Bu(k))^T = u(k)^T B^T
 
-  m.addConstr(C*C_dummy == 1) # To achieve same effect as 1/C
-  m.addConstr(C <= 300)
+    # Constraints in order to obey existing phases
+    m.addConstr(u[k, 0] == u[k, 1]) # Green time of Katipunan Ave North and South must be the same
+    m.addConstr(u[k, 3] == u_41+u_43) # Aurora East has 2 green times/phases
+    m.addConstr(u[k, 2] == u_43-u_41-3) # Green time of Aurora West
+    m.addConstr(C == u[k, 0] + u_43 + 6) # Total cycle time is equal to phase 1 + phase 2 + phase 3
 
-  # Intermediate variable to compute Q and R norm
-  #m.addConstr(y[k, :] == x[k, :] - x[0, :])
-  m.addConstr(y[k, :] == x[k, :] - xref)
-  m.addConstr(z[k, :] == u[k, :] - u[0, :])
-#m.addConstr(y[N, :] == x[N, :] - x[0, :])
-m.addConstr(y[N, :] == x[N, :] - xref)
+    m.addConstr(C*C_dummy == 1) # To achieve same effect as 1/C
+    m.addConstr(C <= 240)
 
-# Objective function
-# Note: Transposition is automatically handled by GUROBI
-obj1 = sum(y[k, :] @ Q @ y[k, :] for k in range(N+1))
-obj2 = sum(z[k, :] @ R @ z[k, :] for k in range(N))
-obj = obj1 + obj2
+    # Intermediate variable to compute Q and R norm
+    #m.addConstr(y[k, :] == x[k, :] - x[0, :])
+    m.addConstr(y[k, :] == x[k, :] - xref)
+    m.addConstr(z[k, :] == u[k, :] - u[0, :])
+  #m.addConstr(y[N, :] == x[N, :] - x[0, :])
+  m.addConstr(y[N, :] == x[N, :] - xref)
 
-# Set objective function
-m.setObjective(obj, GRB.MINIMIZE)
+  # Objective function
+  # Note: Transposition is automatically handled by GUROBI
+  obj1 = sum(y[k, :] @ Q @ y[k, :] for k in range(N+1))
+  obj2 = sum(z[k, :] @ R @ z[k, :] for k in range(N))
+  obj = obj1 + obj2
 
-# Check for infeasibility / unboundedness
-m.setParam("DualReductions", 0) # Infeasible model check
+  # Set objective function
+  m.setObjective(obj, GRB.MINIMIZE)
 
-# Run MPC
-m.reset(0) # Clear previous results
-m.params.NonConvex = 2 # To allow division operator
-m.setParam('TimeLimit', 30) # 30s time limit
-#m.feasRelaxS(0, True, False, False) # Ease model if infeasible
-m.optimize()
+  # Check for infeasibility / unboundedness
+  m.setParam("DualReductions", 0) # Infeasible model check
 
-# Obtain the results of the optimization
-names_to_retrieve = ["C", "u_41", "u_43"]
-vals = {}
-u_res = []
+  # Run MPC
+  m.reset(0) # Clear previous results
+  m.params.NonConvex = 2 # To allow division operator
+  m.setParam('TimeLimit', 30) # 30s time limit
+  #m.feasRelaxS(0, True, False, False) # Ease model if infeasible
+  m.optimize()
 
-# Only get values of u, x, and C
-for i in range(N):
-  for j in range(4):
-    names_to_retrieve.append(f"u[{i},{j}]")
-    names_to_retrieve.append(f"x[{i},{j}]")
+  # Obtain the results of the optimization
+  names_to_retrieve = ["C", "u_41", "u_43"]
+  vals = {}
+  u_res = []
 
-if m.Status == GRB.INFEASIBLE:
-  m.computeIIS() # Check for constraints leading to infeasibility
-  m.write("iis.ilp")
-else:
-  for v in m.getVars():
-    if v.VarName in names_to_retrieve:
-      vals[v.VarName] = v.X
-      print('%s %g' % (v.VarName, v.X))
-      if v.VarName[:3] == "u[0" or (v.VarName == "u_41" or v.VarName == "u_43"):
-        u_res.append(v.X)
+  # Only get values of u, x, and C
+  for i in range(N):
+    for j in range(4):
+      names_to_retrieve.append(f"u[{i},{j}]")
+      names_to_retrieve.append(f"x[{i},{j}]")
 
-  # Post-processing of data
-  for i in range(len(u_res)):
-    if u_res[i] < 0:
-      print(f"Negative green time detected!")
-      break
-    u_res[i] = int(u_res[i]+0.5) # Convert to integer
+  if m.Status == GRB.INFEASIBLE:
+    m.computeIIS() # Check for constraints leading to infeasibility
+    m.write("iis.ilp")
+  else:
+    for v in m.getVars():
+      if v.VarName in names_to_retrieve:
+        vals[v.VarName] = v.X
+        #print('%s %g' % (v.VarName, v.X))
+        if v.VarName[:3] == "u[0" or (v.VarName == "u_41" or v.VarName == "u_43"):
+          u_res.append(v.X)
 
-  vals['C'] = int(vals['C']+0.5) # Convert to integer
+    # Post-processing of data
+    for i in range(len(u_res)):
+      if u_res[i] < 0:
+        print(f"Negative green time detected!")
+        break
+      u_res[i] = int(u_res[i]+0.5) # Convert to integer
 
-  print(f"u[0,:] = {u_res}")
-  print(f"C = {vals['C']}")
+    vals['C'] = int(vals['C']+0.5) # Convert to integer
 
-#print(f"xmin = {xmin}")
-#print(f"xmax = {xmax}")
-#print(f"umin = {umin}")
-#print(f"umax = {umax}")
-#print(f"B = {B}")
-#print(f"D = {D}")
-#print(f"Q = {Q}")
-#print(f"R = {R}")
+    #print(f"xmin = {xmin}")
+    #print(f"xmax = {xmax}")
+    #print(f"umin = {umin}")
+    #print(f"umax = {umax}")
+    #print(f"B = {B}")
+    #print(f"D = {D}")
+    #print(f"Q = {Q}")
+    #print(f"R = {R}")
+
+    return u_res, vals['C']
+  
+def get_timer_settings(u, C):
+  
+  # Amber time (3s, fixed)
+  A = 3
+  
+  # Green times of relevant roads
+  u_katip_s = u[0] # Green time for Katipunan South for phase 1
+  u_katip_n = u[1] # Green time for Katipunan North for phase 1
+  u_aurora_w = u[2] # Green time of Aurora West for phase 3
+  u_aurora_e_katip_s = u[4] # Green time of Aurora East for phase 2
+  u_aurora_e_aurora_w = u[5] # Green time of Aurora East for phase 3
+
+  # Red times of relevant roads
+  r_katip_s = C-A-u_katip_s # Red time for Katipunan South after phase 1
+  r_katip_n = r_katip_s # Red time for Katipunan North after phase 1
+  r_aurora_w = u_katip_s+u_aurora_e_katip_s+2*A # Red time of Aurora West just before phase 3
+  r_aurora_e_katip_s_1 = u_katip_s+A # Red time of Aurora East before phase 2
+  r_aurora_e_katip_s_2 = C-(u_katip_s+u_aurora_e_katip_s+2*A) # Red time of Aurora East after phase 2
+  r_aurora_e_aurora_w = u_katip_s+A# Green time of Aurora East for phase 3
+
+  u_sorted = [u_katip_s, u_katip_n, u_aurora_w, u_aurora_e_katip_s, u_aurora_e_aurora_w]
+  r_sorted = [r_katip_s, r_katip_n, r_aurora_w, r_aurora_e_katip_s_1, r_aurora_e_katip_s_2,
+              r_aurora_e_aurora_w]
+  '''
+  print(f"u_katip_s = {u_katip_s}")
+  print(f"u_katip_n = {u_katip_n}")
+  print(f"u_aurora_w = {u_aurora_w}")
+  print(f"u_aurora_e_katip_s = {u_aurora_e_katip_s}")
+  print(f"u_aurora_e_aurora_w = {u_aurora_e_aurora_w}")
+
+  print(f"r_katip_s = {r_katip_s}")
+  print(f"r_katip_n = {r_katip_n}")
+  print(f"r_aurora_w = {r_aurora_w}")
+  print(f"r_aurora_e_katip_s_1 = {r_aurora_e_katip_s_1}")
+  print(f"r_aurora_e_katip_s_2 = {r_aurora_e_katip_s_2}")
+  print(f"r_aurora_e_aurora_w = {r_aurora_e_aurora_w}")
+  '''
+  return u_sorted, r_sorted
+  
+
+def main():
+  u, C = do_mpc()
+  print(f"u[0,:] = {u}")
+  print(f"C = {C}")
+  get_timer_settings(u, C)
+
+if __name__ == "__main__":
+  main()
